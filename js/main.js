474872e8646ca3cbe871fa5828fdd187e357ede0
Juan Marino Rova <maarino89@hotmail.com>
2024-10-31 09:15:01 -0300

    1.0

--- js/main.js
+++ js/main.js
@@ -1,27 +1,18 @@
-
-
-
-let idx = 0; 
-const images = 2; 
+let idx = 0;
+const images = 2;
 
 setInterval(() => {
-    
-    const currentImg = document.getElementById(`img${idx}`);
-    currentImg.classList.toggle("main_image_show");
-    currentImg.classList.toggle("main_image");
+  const currentImg = document.getElementById(`img${idx}`);
+  currentImg.classList.toggle("main_image_show");
+  currentImg.classList.toggle("main_image");
 
-    idx++;
-    
-    if (idx > images) {
-        idx = 0;
-    }
-    
-    const nextImg = document.getElementById(`img${idx}`);
-    nextImg.classList.toggle("main_image_show");
-    nextImg.classList.toggle("main_image");
+  idx++;
 
+  if (idx > images) {
+    idx = 0;
+  }
+
+  const nextImg = document.getElementById(`img${idx}`);
+  nextImg.classList.toggle("main_image_show");
+  nextImg.classList.toggle("main_image");
 }, 4000);
-
-
-
-
